4e6b30e7ac62d5702fea88af25171d8f0e4d3d91
nico <n.canete01@ufromail.cl>
2022-04-13 11:07:26 -0400

    6

---
+++ src/test/java/TallerTest.java
@@ -0,0 +1,49 @@
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TallerTest {
+    @BeforeAll
+
+    double [ ][ ] datosTesting = new double[][]{
+            {2.5, 3.0, 2.7, 3.8, 4.0, 3.1, 4.6, 2.3, 3.2, 3.1, 2.5, 2.2, 2.5, 3.0, 2.7, 3.8, 4.0, 3.1, 4.5, 2.3, 3.2, 3.1, 2.5, 2.2},
+            {4.5, 2.3, 3.2, 3.1, 2.5, 2.2, 2.5, 3.0, 2.7, 3.8, 4.0, 3.1, 2.5, 2.2, 2.5, 3.0, 2.7, 3.8, 2.5, 3.0, 2.7, 3.8, 4.0, 3.1},
+            {2.3, 3.2, 3.1, 2.5, 2.2, 2.5, 2.2, 2.5, 3.0, 2.7, 3.8, 2.5, 3.0, 2.7, 3.8, 4.0, 3.1, 3.8, 4.0, 3.1, 4.5, 2.3, 3.2, 2.9},
+            {2.5, 3.0, 2.7, 3.8, 4.0, 3.1, 4.5, 2.3, 3.2, 3.1, 2.5, 2.2, 2.5, 2.2, 2.5, 3.0, 2.7, 3.8, 2.7, 3.8, 4.5, 2.3, 3.2, 3.1},
+            {3.8, 4.0, 3.1, 4.5, 2.3, 3.2, 2.5, 2.2, 2.5, 3.0, 2.7, 3.8, 4.5, 2.3, 3.2, 3.1, 2.5, 2.2, 2.5, 3.0, 2.7, 3.8, 4.0, 3.1}
+    };
+
+    @Test
+    void matriz() {
+    }
+
+    @Test
+    void escala() {
+    }
+
+    @Test
+    void valoresMatriz() {
+    }
+
+    @Test
+    void mayorSismo() {
+    }
+
+    @Test
+    void cualMayorSismo() {
+    }
+
+    @Test
+    void sismosAldia4() {
+    }
+
+    @Test
+    void sismoMayor4() {
+    }
+
+    @Test
+    void dias() {
+    }
+}
